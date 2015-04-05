fe0f732d02a2cc75dba25576591da6858a68b677
Daniel Vela <dani_vela@me.com>
2015-04-05 08:08:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RainbowDemo
-Create custom control using IBDesignable in Xcode 6
+
+This demo was made following this tutorial: [Create custom control using IBDesignable in Xcode 6](http://www.appcoda.com/ibdesignable-ibinspectable-tutorial/)
+
+![Xcode IBDesginable Demo](capture.png "Screen capture")
---
+++ RainbowDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainbowDemo
+//
+//  Created by Daniel Vela on 05/04/15.
+//  Copyright (c) 2015 veladan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
